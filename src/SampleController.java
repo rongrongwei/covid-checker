eb4e1a0469c683be750f8eb16e81e2599bfccc1e
MichRich25 <67449190+MichRich25@users.noreply.github.com>
2020-07-29 09:53:08 -0500

    Add files via upload

---
+++ src/SampleController.java
@@ -0,0 +1,69 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class SampleController {
+	 @FXML
+	    private Button signIn;
+
+	    @FXML
+	    private Button Admin;
+
+	    @FXML
+	    private Button createNew;
+
+	    @FXML
+	    void toSignInPage(ActionEvent event) {
+	    	try{
+	    		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("signIn.fxml"));
+	    		Parent root1 = (Parent) fxmlLoader.load();
+	    		Stage stage = new Stage();
+	    		stage.setTitle("Sign-In");
+	    		stage.setScene(new Scene(root1));
+	    		stage.show();
+
+	   			((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+	    	} catch (Exception e){
+	    		System.out.println("Cant load new window");
+	    	}
+	    }
+
+	    @FXML
+	    void toCreateNewPage(ActionEvent event) {
+	    	try{
+	    		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("createNew.fxml"));
+	    		Parent root1 = (Parent) fxmlLoader.load();
+	    		Stage stage = new Stage();
+	    		stage.setTitle("Create New");
+	    		stage.setScene(new Scene(root1));
+	    		stage.show();
+
+	   			((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+	    	} catch (Exception e){
+	    		System.out.println("Cant load new window");
+	    	}
+
+	    }
+
+	    @FXML
+	    void toAdminPage(ActionEvent event) {
+	    	try{
+	    		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Admin.fxml"));
+	    		Parent root1 = (Parent) fxmlLoader.load();
+	    		Stage stage = new Stage();
+	    		stage.setTitle("Administrator");
+	    		stage.setScene(new Scene(root1));
+	    		stage.show();
+
+	   			((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+	    	} catch (Exception e){
+	    		System.out.println("Cant load new window");
+	    	}
+	    }
+}
