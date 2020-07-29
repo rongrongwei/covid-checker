ddd040901205f9497cd6b639da134bf7944092eb
rongrongwei <67390199+rongrongwei@users.noreply.github.com>
2020-07-28 21:19:36 -0500

    New tests for saveSurveyList and loadSurveyList

--- src/TestDataStore.java
+++ src/TestDataStore.java
@@ -3,10 +3,11 @@ import java.util.ArrayList;
 public class TestDataStore {
 
 	public static void main(String[] args) throws Exception {
-		DataStore datastore = new DataStore("mypassword ", "keyfile", "employeefile"); // maybe set up to auto-load employees???
+		DataStore datastore = new DataStore("mypassword ", "keyfile", "employeefile", "surveyfile"); // maybe set up to auto-load employees???
 		
 		ArrayList<Employee> employeeList = new ArrayList<Employee>();
 
+		// test employees
 		Employee testEmployee = new Employee("abc123");
 		testEmployee.setFirstName("Rongrong");
 		testEmployee.setLastName("Wei");
@@ -17,6 +18,7 @@ public class TestDataStore {
 		testEmployee2.setLastName("Smith");
 		employeeList.add(testEmployee2);
 		
+		
 		datastore.saveEmployees(employeeList);
 		
 		ArrayList<Employee> employeeList2 = new ArrayList<Employee>();
@@ -24,6 +26,23 @@ public class TestDataStore {
 		Employee me = employeeList2.get(1);
 		System.out.println(me.getFirstName());
 		
+		// test surveys
+		Survey testSurvey = new Survey("abc123", "07/28/2020", "San Antonio", "98.6", true, true, true);
+		Survey testSurvey2 = new Survey("abc123", "07/27/2020", "San Antonio", "98.4", false, true, true);
+
+		
+		ArrayList<Survey> surveyList = new ArrayList<Survey>();
+		surveyList.add(testSurvey);
+		surveyList.add(testSurvey2);
+		datastore.saveSurveysList(surveyList);
+		
+		ArrayList<Survey> surveyList2 = datastore.loadSurveysList();
+		Survey mySurvey = surveyList2.get(0);
+		System.out.println(mySurvey.getTravel14Days());
+		
+		Survey mySurvey2 = surveyList2.get(1);
+		System.out.println(mySurvey2.getTravel14Days());
+
 	}
 
 }
