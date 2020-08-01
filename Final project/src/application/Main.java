b053709c5fd6c22d0fc4595d7f17ddf50844942b
MichRich25 <67449190+MichRich25@users.noreply.github.com>
2020-07-31 22:38:29 -0500

    Update Main.java

--- Final project/src/application/Main.java
+++ Final project/src/application/Main.java
@@ -1,55 +1,27 @@
 package application;
-
-
-import java.util.ArrayList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+	
+import javafx.application.Application;
 import javafx.stage.Stage;
-import model.DataStore;
-import model.Survey;
-import model.TestDataStore;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
 
 
-public class AdminController {
-	
-	@FXML 
-	TextArea resultsTextArea;
-	
-	@FXML
-	Button resultsButton;
-	private DataStore data;
-	private Survey survey;
-	private TestDataStore test;
-	private ArrayList<Survey> arr = new ArrayList<Survey>();
-	
-	
-	@FXML
-	public void resultsButtonAction(ActionEvent event) throws Exception{
-		arr =(data.loadSurveysList());
-		survey =arr.get(0);
-		resultsTextArea.setText(survey.getEmployeeId()+ " "+survey.getSurveyDate()+" "+survey.getLocation()+" "+survey.getTemperatureValue()+" "+survey.getTravel14Days()+" "+survey.getCovidSymptoms()+" "+survey.getCovidContact());
-		
-		
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("frontPage.fxml"));
+			Scene scene = new Scene(root,500,300);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	@FXML
-    void goBack(ActionEvent event) {
-    	try{
-    		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("frontPage.fxml"));
-    		Parent root1 = (Parent) fxmlLoader.load();
-    		Stage stage = new Stage();
-    		stage.setTitle("Survey");
-    		stage.setScene(new Scene(root1));
-    		stage.show();
-
-   			((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
-    	} catch (Exception e){
-    		System.out.println("Cant load new window");
-    	}
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
